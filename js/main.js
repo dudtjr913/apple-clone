7d3f67d48a6bfc8f62b0c55f6137b9c7c203c51f
unknown <dudtjr913@naver.com>
2021-01-03 20:57:15 +0900

    feat : 스크롤 시 화면에 보이는 scene 표시
    <description>

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 (() => {
+  let currentScene = 0; // 현재 보고 있는 scene
+  let prevScrollHeight = 0; // 보고 있는 scene 전까지의 높이
+  let yOffset = 0; // pageYOffset
+
   const sceneInfo = [
     {
       type: 'sticky',
@@ -54,6 +58,26 @@
     });
   };
 
+  const setScrollLoop = () => {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+    }
+  };
+
   window.addEventListener('resize', debounce(setLayout));
+  window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset;
+    setScrollLoop();
+  });
   setLayout();
 })();
